ac7083af3ce48fef0028df8e0b6be44a8bfd5d92
Pepe <jose.olivert@altumproyectos.com>
2022-05-28 13:17:43 +0200

    Obtener de la API una imagen random

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
-import logo from './logo.svg';
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 import './App.css';
 
-function App() {
+export default function App() {
+  const [data, setData] = useState({ hits: [] });
+
+  const fetchData = async () => {
+    const result = await axios("https://dog.ceo/api/breeds/image/random");
+    setData(result.data);
+    console.log(result);
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1> Imagen random</h1>
+
+      <img id="dog" src={data.message} />
     </div>
   );
 }
-
-export default App;
