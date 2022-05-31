646a1c83a81d19615cd0824d5bc0aa3f4a93d15c
Pepe <jose.olivert@altumproyectos.com>
2022-05-31 13:28:12 +0200

    Añadido botón para seleccionar nueva imagen al pulsar

--- src/App.js
+++ src/App.js
@@ -1,25 +1,35 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
-import './App.css';
+import "./App.css";
 
 export default function App() {
-  const [data, setData] = useState({ hits: [] });
+  const [estado,setEstado]= useState(true)
+  const [data, setData] = useState({message:[]});
 
   const fetchData = async () => {
     const result = await axios("https://dog.ceo/api/breeds/image/random");
     setData(result.data);
-    console.log(result);
+    
   };
 
   useEffect(() => {
     fetchData();
-  }, []);
-  
+    
+  }, [estado]);
+
   return (
-    <div>
+   
+   
+   <div>
       <h1> Imagen random</h1>
 
       <img id="dog" src={data.message} />
+
+      <p>
+        
+        <button onClick={() => setEstado(!estado)}> Push to get new image </button>
+      </p>
+      <h2>La URL de la imagen es {data.message}</h2>
     </div>
   );
 }
