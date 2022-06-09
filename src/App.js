f48d6168ed6d7b3dbd9c68dc781e69b64042f2f9
Pepe <jose.olivert@altumproyectos.com>
2022-06-09 17:31:43 +0200

    leida lista de todas las razas

--- src/App.js
+++ src/App.js
@@ -3,33 +3,55 @@ import axios from "axios";
 import "./App.css";
 
 export default function App() {
-  const [estado,setEstado]= useState(true)
-  const [data, setData] = useState({message:[]});
+  const [estado, setEstado] = useState(true);
+  const [estado2, setEstado2] = useState(true);
+  const [data, setData] = useState({ message: [] });
+  const [data1, setData1] = useState({ message: [] });
 
   const fetchData = async () => {
     const result = await axios("https://dog.ceo/api/breeds/image/random");
     setData(result.data);
-    
+  };
+
+  const fetchAll = async () => {
+    const lista = await axios("https://dog.ceo/api/breeds/list/all");
+
+    setData1(lista.data);
   };
 
   useEffect(() => {
     fetchData();
-    
   }, [estado]);
 
+  useEffect(() => {
+    fetchAll();
+  }, [estado2]);
+
   return (
-   
-   
-   <div>
+    <div>
       <h1> Imagen random</h1>
 
       <img id="dog" src={data.message} />
 
       <p>
-        
-        <button onClick={() => setEstado(!estado)}> Push to get new image </button>
+        <button onClick={() => setEstado(!estado)}>
+          {" "}
+          Push to get new image{" "}
+        </button>
       </p>
       <h2>La URL de la imagen es {data.message}</h2>
+
+      <p>
+        <button onClick={() => setEstado2(!estado2)}>
+          Push to get all breeds list
+        </button>
+      </p>
+
+      <ul>
+        {Object.keys(data1.message).map((item) => (
+          <li>{item}</li>
+        ))}
+      </ul>
     </div>
   );
 }
